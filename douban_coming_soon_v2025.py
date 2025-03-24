5a273213a474dc2964dee7b3bf719b03a9c8ccc7
redpandacat <redpandanat@users.noreply.github.com>
2025-03-24 17:43:24 +0800

    Update douban_coming_soon_v2025.py

--- douban_coming_soon_v2025.py
+++ douban_coming_soon_v2025.py
@@ -187,8 +187,8 @@ def format_release_date(date):
 df_combined["release_date"] = df_combined["release_date"].apply(format_release_date)
 print(df_combined.head())  # This will show a few rows of the DataFrame to inspect the content.
 print(df_combined["old_release_date"])
-df_combined["old_release_date"] = df_combined["old_release_date"].astype(str)
-df_combined["old_release_date"] = df_combined["old_release_date"].apply(lambda x: x.replace('-99', '-??') if isinstance(x, str) else x)
+#df_combined["old_release_date"] = df_combined["old_release_date"].astype(str)
+#df_combined["old_release_date"] = df_combined["old_release_date"].apply(lambda x: x.replace('-99', '-??') if isinstance(x, str) else x)
 
 #df_combined["old_release_date"] = df_combined["old_release_date"].apply(format_release_date)
 
